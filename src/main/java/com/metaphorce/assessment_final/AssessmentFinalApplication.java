a1ee17ce5ad0fff9c4a3512532db72ba69956dd9
SayulRamirez <sayulramirez@gmail.com>
2024-07-24 23:33:11 -0600

    Creando el proyecto de Gestión de tareas para el Assessment final del Career Booster de Metaphorce

---
+++ src/main/java/com/metaphorce/assessment_final/AssessmentFinalApplication.java
@@ -0,0 +1,13 @@
+package com.metaphorce.assessment_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssessmentFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/metaphorce/assessment_final/AssessmentFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metaphorce.assessment_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
