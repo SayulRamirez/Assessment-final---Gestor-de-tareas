8fb77eb29ce18175bbae9509f0b0e6497feb4cc3
SayulRamirez <sayulramirez@gmail.com>
2024-07-30 22:50:32 -0600

    Agregado el metodo findResponsibleByProject al repositorio de Task para buscar a los usuarios asociados al proyecto

--- src/main/java/com/metaphorce/assessment_final/repositories/TaskRepository.java
+++ src/main/java/com/metaphorce/assessment_final/repositories/TaskRepository.java
@@ -1,7 +1,10 @@
 package com.metaphorce.assessment_final.repositories;
 
+import com.metaphorce.assessment_final.dto.TaskStatusCount;
 import com.metaphorce.assessment_final.entities.Task;
+import com.metaphorce.assessment_final.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +15,10 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findAllByResponsibleId(Long id);
 
     void deleteAllByProjectId(Long id);
+
+    @Query("select t.status as status, count(t.status) as count from Task t where t.responsible.id =:id group by t.status")
+    List<TaskStatusCount> countTaskByStatus(Long id);
+
+    @Query("select distinct t.responsible from Task t where t.project.id =:id")
+    List<User> findResponsibleByProjectId(Long id);
 }
