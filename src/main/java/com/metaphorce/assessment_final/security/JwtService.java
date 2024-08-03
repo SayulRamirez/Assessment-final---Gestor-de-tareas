6930633510fccc4b9b63a6bdb4301f13776e5f40
SayulRamirez <sayulramirez@gmail.com>
2024-08-03 14:13:06 -0600

    Creado el servicio para la generacion del JWT y la validacion del mismo

---
+++ src/main/java/com/metaphorce/assessment_final/security/JwtService.java
@@ -0,0 +1,78 @@
+package com.metaphorce.assessment_final.security;
+
+import com.metaphorce.assessment_final.entities.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = "vfa7tABfABAf9va35638sGBw35bgb4g4btb5twGnyYu6uhw6wb3bghSgnJ5yh6bw4bgs5bsE5tgf55bdf5vb1by4u7753snb34b5gbs6yh4gb6b4tghGggDdfGHSSJyRbfBeba";
+
+    public String getToken(User user) {
+
+        return getToken(new HashMap<>(), user);
+    }
+
+    private String getToken(Map<String, Object> extraClaims, User user) {
+
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setId(user.getId().toString())
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getKey(), SignatureAlgorithm.HS256).compact();
+    }
+
+    private Key getKey() {
+
+        byte[] keyByte = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyByte);
+    }
+
+    public String getEmailFromToken(String token) {
+        return getClaim(token, Claims::getSubject);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+
+        final String email = getEmailFromToken(token);
+
+        return (email.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private Claims getAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public <T> T getClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaims(token);
+
+        return claimsResolver.apply(claims);
+    }
+
+    private Date getExpiration(String token) {
+        return getClaim(token, Claims::getExpiration);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return getExpiration(token).before(new Date());
+    }
+}
