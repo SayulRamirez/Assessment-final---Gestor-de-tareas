9e1605e2d5b0563182e2fc3739df707ba8acddd4
SayulRamirez <sayulramirez@gmail.com>
2024-08-03 14:20:59 -0600

    Creados los endpoints para el registro y el login de la aplicacion

---
+++ src/main/java/com/metaphorce/assessment_final/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.metaphorce.assessment_final.controller;
+
+import com.metaphorce.assessment_final.dto.AuthResponse;
+import com.metaphorce.assessment_final.dto.LoginRequest;
+import com.metaphorce.assessment_final.dto.RegisterRequest;
+import com.metaphorce.assessment_final.security.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping(value = "login")
+    public ResponseEntity<AuthResponse> login(@RequestBody @Valid LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+    @PostMapping(value = "register")
+    public ResponseEntity<AuthResponse> register(@RequestBody @Valid RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+}
