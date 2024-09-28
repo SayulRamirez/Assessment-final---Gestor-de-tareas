7db074803855e0713e3973c82eb6475bad6fea77
SayulRamirez <sayulramirez@gmail.com>
2024-09-27 21:02:05 -0600

    feat: agregando metodos por defecto a la interface TaskStatusCount

--- src/main/java/com/metaphorce/assessment_final/dto/TaskStatusCount.java
+++ src/main/java/com/metaphorce/assessment_final/dto/TaskStatusCount.java
@@ -7,4 +7,16 @@ public interface TaskStatusCount {
     Status getStatus();
 
     int getCount();
+
+    default boolean isPending() {
+        return getStatus() == Status.PENDING;
+    }
+
+    default boolean isInProgress() {
+        return getStatus() == Status.IN_PROGRESS;
+    }
+
+    default boolean isCompleted() {
+        return getStatus() == Status.COMPLETE;
+    }
 }
